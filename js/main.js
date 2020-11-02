3d1611509f77958a761b45cffbdcc926e1ea6c66
Jeremy Durden <jeremydurden@gmail.com>
2020-11-02 10:55:21 -0800

    writing pseudo-code

--- js/main.js
+++ js/main.js
@@ -14,6 +14,8 @@
 
 
 
+
+
 //============ Cached ========================//
 
 
@@ -35,5 +37,39 @@
 //============Functions===================//
 
 
+//creating a random array of bombs that can correspond with the cells of my grid
+
+
+
+//a timer that counts up by seconds and minutes to track how long it takes to finish
+
+
+
+//using an click listenering and checking if the cell is a bomb - if it is you lose
+//otherwise move on to check neighboring cells
+
+
+
+//checking if neighboring cells are bombs and clearing up to the edge of where they are
+//touching a bomb and then numbering how many bombs they are touching
+//checking if I am on an edge or not and how to know which surrounding cells to check
+
+
+
+//right click to mark with a flag, ?, or clear
+
+
+
+
+//display win or lose messages with total time
+
+
+
+
+
+
+
+
+//init function
 
 
